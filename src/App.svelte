c92553b98ace79bfc3245b32f86d899f31eb4ba0
Tarık Coşkun <imtarikcoskun@gmail.com>
2021-07-31 13:08:39 +0300

    image borders in a "convenient" way

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,67 @@
   import ChapterC from "./levels/chapter-c";
   import ChapterD from "./levels/chapter-d";
   import ChapterE from "./levels/chapter-e";
+
+  let edgeSize = 200;
+  let timer = null;
+
+  function scrollOnEdges(event) {
+    let viewportX = event.clientX;
+    let viewportWidth = document.documentElement.clientWidth;
+    let edgeLeft = edgeSize;
+    let edgeRight = viewportWidth - edgeSize;
+    let isInLeftEdge = viewportX < edgeLeft;
+    let isInRightEdge = viewportX > edgeRight;
+
+    if (!(isInLeftEdge || isInRightEdge)) {
+      clearTimeout(timer);
+      return;
+    }
+
+    let documentWidth = Math.max(
+      document.body.scrollWidth,
+      document.body.offsetWidth,
+      document.body.clientWidth,
+      document.documentElement.scrollWidth,
+      document.documentElement.offsetWidth,
+      document.documentElement.clientWidth
+    );
+
+    let maxScrollX = documentWidth - viewportWidth;
+
+    (function checkForWindowScroll() {
+      clearTimeout(timer);
+
+      if (adjustWindowScroll()) {
+        timer = setTimeout(checkForWindowScroll, 30);
+      }
+    })();
+
+    function adjustWindowScroll() {
+      let currentScrollX = window.pageXOffset;
+      let canScrollLeft = currentScrollX > 0;
+      let canScrollRight = currentScrollX < maxScrollX;
+      let nextScrollX = currentScrollX;
+      let maxStep = 50;
+      if (isInLeftEdge && canScrollLeft) {
+        let intensity = (edgeLeft - viewportX) / edgeSize;
+
+        nextScrollX = nextScrollX - maxStep * intensity;
+      } else if (isInRightEdge && canScrollRight) {
+        let intensity = (viewportX - edgeRight) / edgeSize;
+
+        nextScrollX = nextScrollX + maxStep * intensity;
+      }
+      nextScrollX = Math.max(0, Math.min(maxScrollX, nextScrollX));
+
+      if (nextScrollX !== currentScrollX) {
+        window.scrollTo(nextScrollX, 0);
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
 </script>
 
 <main class="points">
@@ -60,5 +121,6 @@
     align-items: center;
     max-width: 100%;
     margin-left: auto;
+    scroll-snap-type: x mandatory;
   }
 </style>
--- src/Chapter.svelte
+++ src/Chapter.svelte
@@ -5,12 +5,27 @@
   let levelsArray = Object.entries(levels).sort((a, b) => {
     return a[1] < b[1] ? -1 : a[1] > b[1] ? 1 : 0;
   });
+
+  function soundEffect() {
+    let sfx = new Audio("/sfx/hover_default");
+    sfx.play();
+  }
 </script>
 
 <section {id}>
   {#each levelsArray as level, index}
-    <figure style={"animation-delay:" + index * 0.25 + "s"}>
-      {#if level[1].secret}
+    <figure
+      style={"animation-delay:" + index * 0.25 + "s"}
+      on:mouseenter={soundEffect}
+    >
+      {#if level[1].exploration}
+        <img
+          class={"exp_point_icon " + level[1].color}
+          src="/img/exploration_point.png"
+          alt="exploration_point"
+        />
+      {/if}
+      <!-- {#if level[1].secret}
         <img class="secret_icon" src="/img/secret.png" alt="secret" />
       {/if}
       {#if level[1].combat}
@@ -19,11 +34,18 @@
           src="/img/difficulty.png"
           alt="difficulty"
         />
-      {/if}
+      {/if} -->
       <img
         class={level[1].boss
           ? "boss_icon " + level[1].color
           : "level_icon " + level[1].color}
+        src={`/img/level_icons/${level[1].icon}.png`}
+        alt={level[1].icon}
+      />
+      <img
+        class={level[1].boss
+          ? "boss_icon level_icon level_icon_ghost " + level[1].color
+          : "level_icon level_icon_ghost " + level[1].color}
         src={`/img/level_icons/${level[1].icon}.png`}
         alt={level[1].icon}
       />
@@ -37,12 +59,26 @@
 </section>
 
 <style lang="scss">
+  @mixin blueFilter {
+    filter: invert(1) invert(65%) sepia(40%) saturate(5396%) hue-rotate(159deg)
+      brightness(101%) contrast(102%);
+  }
+  @mixin pinkFilter {
+    filter: invert(1) invert(61%) sepia(59%) saturate(4618%) hue-rotate(260deg)
+      brightness(106%) contrast(103%);
+  }
+  @mixin orangeFilter {
+    filter: invert(1) invert(90%) sepia(14%) saturate(6503%) hue-rotate(332deg)
+      brightness(101%) contrast(103%);
+  }
+
   section {
     position: relative;
     display: flex;
     align-items: center;
     justify-content: center;
     height: 100px;
+    scroll-snap-align: start;
     &::before {
       content: "";
       position: absolute;
@@ -62,7 +98,10 @@
       bottom: calc(14rem + 16px);
     }
     &#A {
-      margin-left: 2.5rem;
+      margin-left: 10rem;
+    }
+    &#E {
+      padding-right: 10rem;
     }
     figure {
       display: flex;
@@ -71,45 +110,75 @@
       align-items: center;
       justify-content: center;
       position: relative;
-      transition: all 0.2s ease-out;
+      transition: 0.25s;
       width: 4rem;
       animation: upDown 6s infinite;
+      .exp_point_icon {
+        position: absolute;
+        top: -3.5rem;
+        width: 36px;
+        transition: 0.25s;
+        &.blue {
+          @include blueFilter();
+        }
+        &.pink {
+          @include pinkFilter();
+        }
+        &.orange {
+          @include orangeFilter();
+        }
+      }
       .secret_icon {
         position: absolute;
         bottom: 4.5rem;
         width: 42px;
         z-index: 10;
         filter: grayscale(1);
-        transition: all 400ms cubic-bezier(0.125, 0.250, 0.100, 1.035);
+        transition: 0.25s;
       }
       .difficulty_icon {
         position: absolute;
         bottom: 10.5rem;
         opacity: 0;
-        transition: all 400ms cubic-bezier(0.125, 0.250, 0.100, 1.035);
+        transition: 0.25s cubic-bezier(0.125, 0.25, 0.1, 1.035);
+      }
+      .boss_icon {
+        width: 60px;
       }
       .level_icon,
       .boss_icon {
-        width: 56px;
+        width: 52px;
         transform: rotate(90deg);
-        transition: all 400ms cubic-bezier(0.125, 0.250, 0.100, 1.035);
-        &.boss_icon {
-          width: 64px;
+        transition: 0.25s cubic-bezier(0.125, 0.25, 0.1, 1.035);
+        &_ghost {
+          position: absolute;
+          margin-top: -48px;
+          width: 62px;
+          z-index: -1;
+          &.blue {
+            filter: invert(1) invert(13%) sepia(95%) saturate(7036%)
+              hue-rotate(237deg) brightness(103%) contrast(108%)
+              drop-shadow(0 0 32px var(--blue-darker));
+          }
+          &.pink {
+            filter: invert(1) invert(22%) sepia(78%) saturate(7153%)
+              hue-rotate(277deg) brightness(102%) contrast(113%)
+              drop-shadow(0 0 32px var(--pink-darker));
+          }
+          &.orange {
+            filter: invert(1) invert(40%) sepia(67%) saturate(6652%)
+              hue-rotate(352deg) brightness(102%) contrast(101%)
+              drop-shadow(0 0 32px var(--orange-darker));
+          }
         }
-        &.blue {
-          filter: invert(1) invert(65%) sepia(40%) saturate(5396%)
-            hue-rotate(159deg) brightness(101%) contrast(102%)
-            drop-shadow(0 0 8px var(--blue-darker));
+        &.blue:not(.level_icon_ghost) {
+          @include blueFilter();
         }
-        &.pink {
-          filter: invert(1) invert(61%) sepia(59%) saturate(4618%)
-            hue-rotate(260deg) brightness(106%) contrast(103%)
-            drop-shadow(0 0 8px var(--pink-darker));
+        &.pink:not(.level_icon_ghost) {
+          @include pinkFilter();
         }
-        &.orange {
-          filter: invert(1) invert(90%) sepia(14%) saturate(6503%)
-            hue-rotate(332deg) brightness(101%) contrast(103%)
-            drop-shadow(0 0 8px var(--orange-darker));
+        &.orange:not(.level_icon_ghost) {
+          @include orangeFilter();
         }
       }
       h1 {
@@ -117,10 +186,13 @@
         letter-spacing: 0.5px;
         margin-bottom: 0.5rem;
         opacity: 0;
-        transition: all 0.2s;
+        transition: 0.2s;
       }
       &:hover {
         padding: 0 2.5rem;
+        .exp_point_icon {
+          width: 54px;
+        }
         .secret_icon {
           filter: drop-shadow(0 0 8px #ffff0090) grayscale(0);
           width: 72px;
@@ -129,13 +201,13 @@
         .difficulty_icon {
           opacity: 100;
         }
-        .level_icon,
-        .boss_icon {
+        .level_icon {
           width: 96px;
           transform: rotate(0);
-          &.boss_icon {
-            width: 104px;
-          }
+        }
+        .boss_icon {
+          width: 104px;
+          transform: rotate(0);
         }
         h1 {
           opacity: 100;
